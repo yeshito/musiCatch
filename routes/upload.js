60c02d13f6f62c59c38d8c22f8fdf0e3705fc900
Yeshe Wingerd <yeshito@gmail.com>
2016-09-21 15:11:46 -0700

    took out unneeded modules/dependencies being used in upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -5,15 +5,6 @@ const path = require('path');
 const busboy = require('connect-busboy');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const xml2js = require('xml2js');
-const parser = new xml2js.Parser({strict: false, trim: true});
-const xmlStream = require('xml-stream');
-// element tree
-const et = require('elementtree');
-const XML = et.XML;
-const ElementTree = et.ElementTree;
-const element = et.Element;
-const subElement = et.SubElement;
 
 router.use(busboy({ immediate: true , limits: { files: 1, fileSize: 10000000 } }));
 
@@ -23,21 +14,17 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+
     let fstream;
     req.pipe(req.busboy);
     req.busboy.on('file', function (fieldname, file, filename) {
       let filePath = __dirname + '/files/' + req.session.user + filename;
-      // console.log(filePath)
+
         fstream = fs.createWriteStream(filePath);
         file.pipe(fstream);
-        // let stream = fs.createReadStream(filePath);
-        // let xml = new xmlStream(stream);
 
         fstream.on('close', function () {
-          // data = fs.readFileSync('document.xml').toString();
-          // etree = et.parse(data);
 
-          // console.log(etree.findall('*'))
           fs.readFile(filePath , (err, data) => {
 
             let artists = data.toString().match(/<key>Artist<\/key><string>(.*?)<\/string>/g);
@@ -60,15 +47,6 @@ router.post('/', (req, res) => {
             let sortedArtists = trimmedArr.sort();
             console.log('sortedArr is: ' + JSON.stringify(sortedArtists));
 
-
-            // parser.parseString(data, (err, result) => {
-            //   let artistArr = [];
-            //   result['PLIST']['DICT'].forEach(el => {
-            //     artistArr.push(el['STRING'][2]);
-            //   });
-            //   console.log(JSON.stringify(artistArr.sort()))
-            //
-            // })
           })
 
         });
