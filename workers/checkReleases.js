53be694e34a2f4eeae839aa57fbaa52a312251aa
Yeshe Wingerd <yeshito@gmail.com>
2016-09-28 00:11:26 -0700

    added demo code for presentation not done

--- workers/checkReleases.js
+++ workers/checkReleases.js
@@ -10,8 +10,200 @@ const fs = require('fs');
 const parseString = require('xml2js').parseString;
 const sendNotifications = require('./sendNotifications.js').sendNotifications;
 
-request('https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/sf=143441/limit=100/explicit=true/rss.xml', (error, response, body) => {
+// request('https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/sf=143441/limit=100/explicit=true/rss.xml', (error, response, body) => {
+//
+//   if(error){
+//         return console.log('Error:', error);
+//     }
+//     //Check for right status code
+//     if(response.statusCode !== 200){
+//         return console.log('Invalid Status Code Returned from release request: ', response.statusCode);
+//     }
+//
+//     //All is good. Print the body
+//     parseString( body, (err, result) => {
+//       if (err) console.log(err);
+//
+//       let newDate = new Date().toString();
+//       let session = driver.session();
+//
+//       session
+//       .run( "MATCH (l:lastReleaseCheck) RETURN l.date AS lastDate")
+//       .then( record => {
+//         let lastDate = record.records[0].get("lastDate");
+//         let dummyDate = 1474320695;
+//
+//         return session.run("MATCH (l:lastReleaseCheck) SET l.date = {date} RETURN l", {date: newDate})
+//         .catch( err => {
+//           console.log(err)
+//         })
+//         .then( record => {
+//
+//           const releasesArr = result.rss.channel[0].item;
+//
+//           // let release0 = result.rss.channel[0].item[0]
+//           // let release1 = result.rss.channel[0].item[1]
+//           let release = result.rss.channel[0].item[15];
+//           console.log(JSON.stringify(release));
+//           // releasesArr.forEach( release => {
+//           // If release is more recent then last time release were checked add to Db
+//             if (Date.parse(release.pubDate[0]) > dummyDate) {
+//
+//               let releaseArtistId = parseInt(release['itms:artistLink'][0].match(/id(\d+)\?/)[1], 10);
+//
+//               return session
+//               .run("CREATE (r:Release { title: {title}, link: {link}, description: {description}, pubDate: {pubDate}, contentEncoded: {contentEncoded}, category: {category}, " +
+//                  "artist: {artist}, artistLink: {artistLink}, album: {album}, albumPrice: {albumPrice}, coverArt: {coverArt}, rights: {rights}, releaseDate: {releaseDate} }) " +
+//                  "RETURN ID(r) AS releaseId",
+//                  { title: release.title[0], link: release.link[0], description: release.description[0], pubDate: release.pubDate[0], contentEncoded: release['content:encoded'][0],
+//                    category: release.category[0]['_'], artist: release['itms:artist'][0], artistLink: release['itms:artistLink'][0], album: release['itms:album'][0], albumLink: release['itms:albumLink'][0],
+//                    albumPrice: release['itms:albumPrice'][0], coverArt: release['itms:coverArt'][2]['_'], rights: release['itms:rights'][0], releaseDate: release['itms:releasedate'][0] })
+//                    .then( record => {
+//                      let releaseId = parseInt(record.records[0].get("releaseId"), 10);
+//                      console.log('releaseId is: ' + releaseId);
+//                      console.log('releaseArtistId is: ' + releaseArtistId);
+//
+//                      return session
+//                      .run( "MATCH (a:Artist) WHERE a.artistId = {releaseArtistId} RETURN a.artistId as appleId", { releaseArtistId: releaseArtistId } )
+//                      .then( record => {
+//                         console.log('record after matching artist' + JSON.stringify(record))
+//
+//                          if (record.records.length === 0) {
+//                           //  console.log('record.records.length === 0 is hit!')
+//                           //  console.log('releaseArtistId is: ' + releaseArtistId)
+//                            request
+//                            .get(`https://itunes.apple.com/lookup?id=${releaseArtistId}`, (error, response, body) => {
+//
+//                              if (error) return console.log('release get artist itunes API error: ' + error);
+//                              if (!error && response.statusCode == 200) {
+//                                let bodyJSON = JSON.parse(body);
+//                                let artistObj = bodyJSON.results[0];
+//                                console.log('typeof artistObj.artistId ' + typeof artistObj.artistId)
+//
+//                              return session.run("CREATE (a:Artist { wrapperType: {wrapperType}, artistType: {artistType}, artistName: {artistName}, artistLinkUrl: {artistLinkUrl}, " +
+//                                                 "artistId: {artistId}, primaryGenreName: {primaryGenreName}, primaryGenreId: {primaryGenreId} })"
+//                                          , { wrapperType: artistObj.wrapperType, artistType: artistObj.artistType, artistName: artistObj.artistName, artistLinkUrl: artistObj.artistLinkUrl,
+//                                             artistId: artistObj.artistId, primaryGenreName: artistObj.primaryGenreName, primaryGenreId: artistObj.primaryGenreId })
+//                                          .then( record => {
+//                                            console.log("record after creating artist" + JSON.stringify(record))
+//                                            console.log("releaseArtistId is: " + releaseArtistId, "releaseId is: " + releaseId)
+//                                            console.log("typeof releaseArtistId: " + typeof releaseArtistId, "typeof releaseId: " + releaseId )
+//                                            return session.run("MATCH (a:Artist { artistId: {artistId} }) " +
+//                                                               "MATCH (r:Release) WHERE ID(r) = " + releaseId + " " +
+//                                                               "MERGE (a)-[:RELEASED]->(r)", { artistId: releaseArtistId })
+//                                        }).then( record => {
+//                                          console.log('record is: ' + JSON.stringify(record));
+//                                            return session.run("MATCH (n:User)-[:LIKES]-(a:Artist { artistId: {artistId} }) " +
+//                                                                "RETURN n", { artistId: 153159 })
+//                                                  .then( record => {
+//                                                    console.log('releaseId is: ' + releaseId)
+//                                                    if(record.records.length > 0) {
+//
+//                                                      record.records.forEach( user => {
+//                                                        let userProps = user['_fields'][0]['properties'];
+//                                                        let notificationObj = { user: { id: user['_fields'][0]['identity']['low'], firstName: userProps.lastName, cellNum: userProps.cellNum, email: userProps.email },
+//                                                                                   release: { name: release['itms:album'][0], artist: release['itms:artist'][0], coverArt: release['itms:coverArt'][2]['_'], link: release['itms:albumLink'][0], } }
+//
+//                                                        redis.lpush('notificationList', JSON.stringify(notificationObj));
+//                                                      })
+//                                                      sendNotifications();
+//                                                      session.close();
+//                                                      driver.close();
+//                                                    }
+//
+//                                                  }).catch( err => {
+//                                                    console.log(err)
+//                                                  })
+//
+//                                        }).catch( err => {
+//                                          console.log(err);
+//                                        })
+//                              };
+//                            });
+//                          } else {
+//                            console.log('releaseId is: ' + releaseId, 'releaseArtistId is: ' + releaseArtistId)
+//                            console.log('typeof releaseId and releaseArtistId is: ' +typeof releaseId + ' ' + typeof releaseArtistId)
+//                            return session.run(
+//                                             "MATCH (a:Artist) WHERE a.artistId = {artistId} " +
+//                                              "MATCH (r:Release) WHERE ID(r) = " + releaseId + " " +
+//                                              "MERGE (a)-[:RELEASED]->(r)", { artistId: releaseArtistId })
+//                              .then( record => {
+//                              console.log('record is: ' + JSON.stringify(record));
+//                                return session.run("MATCH (n:User)-[:LIKES]-(a:Artist { artistId: {artistId} }) " +
+//                                                    "RETURN n", { artistId: 153159 })
+//                                      .then( record => {
+//                                        console.log('releaseId is: ' + releaseId)
+//                                        if(record.records.length > 0) {
+//
+//                                          record.records.forEach( user => {
+//                                            let userProps = user['_fields'][0]['properties'];
+//                                            let notificationObj = { user: { id: user['_fields'][0]['identity']['low'], firstName: userProps.lastName, cellNum: userProps.cellNum, email: userProps.email },
+//                                                                       release: { name: release['itms:album'][0], artist: release['itms:artist'][0], coverArt: release['itms:coverArt'][2]['_'], link: release['itms:albumLink'][0], } }
+//
+//                                            redis.lpush('notificationList', JSON.stringify(notificationObj));
+//                                          })
+//                                          sendNotifications();
+//                                          session.close();
+//                                          driver.close();
+//                                        }
+//
+//                                      }).catch( err => {
+//                                        console.log(err)
+//                                      })
+//                            }).catch( err => {
+//                              console.log('here is the error!')
+//                              console.log(err);
+//                            })
+//                          }
+//
+//                      }).catch(err => {
+//                        console.log(err)
+//                      }).then( record => {
+//                        // getArtistId();
+//                        // session.close();
+//                        // driver.close();
+//                      })
+//                        })
+//             }
+//           })
+//           session.close();
+//           driver.close();
+//         }).catch(err => {
+//
+//
+//           console.log(err)
+//         })
+//
+//       })
+//
+//     })
+// })
 
+
+// FOR DEMO
+
+// CREATE CLASSMATE USERS
+// const g26Class = [{ firstName: 'Yeshe', cellNum: '707-364-8042'}, { firstName: 'Clint', cellNum: '415-702-0623'}, { firstName: 'Dai', cellNum: '310-800-3568'}, { firstName: 'Nica', cellNum: '925-348-0347'},
+//                   { firstName: 'Ian', cellNum: '714-404-9641'}, { firstName: 'James', cellNum: '714-767-8787'}, { firstName: 'Kun', cellNum: '347-479-4743'}, { firstName: 'Monique', cellNum: '504-237-1383'},
+//                   { firstName: 'Ono', cellNum: '619-940-8675'}, { firstName: 'Matt', cellNum: '530-615-7446'}, { firstName: 'Liz', cellNum: '682-597-8986'}, { firstName: 'Alex', cellNum: '562-552-2378' },
+//                   { firstName: 'Winnie', cellNum: '415-298-2139'}]
+// let session = driver.session();
+//
+// g26Class.forEach( user => {
+//   session.run("CREATE (u:User { firstName: {firstName}, cellNum: {cellNum}, signupDate: {signupDate} }) " +
+//               "RETURN id(u) as id"
+//               , { firstName: user.firstName, cellNum: user.cellNum, signupDate: new Date().toString() })
+//   .then( result => {
+//     console.log(JSON.stringify(result))
+//   })
+//   .catch( err => {
+//     console.log(err)
+//   })
+// })
+// END CREATE CLASSMATE USERS
+
+request('https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/sf=143441/limit=100/explicit=true/rss.xml', (error, response, body) => {
+  let j = 0;
   if(error){
         return console.log('Error:', error);
     }
@@ -41,11 +233,7 @@ request('https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/sf
 
           const releasesArr = result.rss.channel[0].item;
 
-          // let release0 = result.rss.channel[0].item[0]
-          // let release1 = result.rss.channel[0].item[1]
-          let release = result.rss.channel[0].item[15];
-          console.log(JSON.stringify(release));
-          // releasesArr.forEach( release => {
+          for(let i = 0; i < 13; i++ ) {
           // If release is more recent then last time release were checked add to Db
             if (Date.parse(release.pubDate[0]) > dummyDate) {
 
@@ -60,8 +248,6 @@ request('https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/sf
                    albumPrice: release['itms:albumPrice'][0], coverArt: release['itms:coverArt'][2]['_'], rights: release['itms:rights'][0], releaseDate: release['itms:releasedate'][0] })
                    .then( record => {
                      let releaseId = parseInt(record.records[0].get("releaseId"), 10);
-                     console.log('releaseId is: ' + releaseId);
-                     console.log('releaseArtistId is: ' + releaseArtistId);
 
                      return session
                      .run( "MATCH (a:Artist) WHERE a.artistId = {releaseArtistId} RETURN a.artistId as appleId", { releaseArtistId: releaseArtistId } )
@@ -92,29 +278,14 @@ request('https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/sf
                                                               "MATCH (r:Release) WHERE ID(r) = " + releaseId + " " +
                                                               "MERGE (a)-[:RELEASED]->(r)", { artistId: releaseArtistId })
                                        }).then( record => {
-                                         console.log('record is: ' + JSON.stringify(record));
-                                           return session.run("MATCH (n:User)-[:LIKES]-(a:Artist { artistId: {artistId} }) " +
-                                                               "RETURN n", { artistId: 153159 })
-                                                 .then( record => {
-                                                   console.log('releaseId is: ' + releaseId)
-                                                   if(record.records.length > 0) {
+                                         console.log('releaseId is: ' + releaseId)
 
-                                                     record.records.forEach( user => {
-                                                       let userProps = user['_fields'][0]['properties'];
-                                                       let notificationObj = { user: { id: user['_fields'][0]['identity']['low'], firstName: userProps.lastName, cellNum: userProps.cellNum, email: userProps.email },
-                                                                                  release: { name: release['itms:album'][0], artist: release['itms:artist'][0], coverArt: release['itms:coverArt'][2]['_'], link: release['itms:albumLink'][0], } }
-
-                                                       redis.lpush('notificationList', JSON.stringify(notificationObj));
-                                                     })
-                                                     sendNotifications();
-                                                     session.close();
-                                                     driver.close();
-                                                   }
-
-                                                 }).catch( err => {
-                                                   console.log(err)
-                                                 })
-
+                                          let curUser = g26Class[j]
+                                          let userProps = user['_fields'][0]['properties'];
+                                          let notificationObj = { user: {firstName: curUser.firstName, cellNum: curUser.cellNum },
+                                                                  release: { name: release['itms:album'][0], artist: release['itms:artist'][0], coverArt: release['itms:coverArt'][2]['_'], link: release['itms:albumLink'][0], } }
+                                          j++;
+                                             redis.lpush('notificationList', JSON.stringify(notificationObj));
                                        }).catch( err => {
                                          console.log(err);
                                        })
@@ -165,7 +336,7 @@ request('https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/sf
                      })
                        })
             }
-          })
+          }
           session.close();
           driver.close();
         }).catch(err => {
@@ -177,4 +348,4 @@ request('https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/newreleases/sf
       })
 
     })
-// })
+  })
